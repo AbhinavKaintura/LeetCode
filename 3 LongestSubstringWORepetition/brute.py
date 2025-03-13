3490171e8f995e502dcef1357ab513ef20756278
AbhinavKaintura <abhinavkaintura001@gmail.com>
2025-03-13 23:40:30 +0530

    3

---
+++ 3 LongestSubstringWORepetition/brute.py
@@ -0,0 +1,16 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s:str) -> str:
+
+        max_length = 0
+        n = len(s)
+
+        for i in range(n):
+            for j in range(i, n):
+                if len(set(s[i:j+1])) == j-i+1:
+                    max_length = max(max_length, j-i+1)
+
+        return max_length
+    
+print(Solution().lengthOfLongestSubstring("abcabcbb")) # 3
+print(Solution().lengthOfLongestSubstring("bbbbb")) # 1
+print(Solution().lengthOfLongestSubstring("pwwkew")) # 3
